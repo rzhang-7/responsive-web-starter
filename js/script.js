964d4f93fce28e7a7638fcbc09fbbc457b0c3b3b
Raymond Zhang <dracorz401@gmail.com>
2023-11-17 18:59:39 -0500

    finished page 1 and 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 let currentTab = 0;
 const tabList = document.getElementsByClassName("tab");
 const stepList = document.getElementsByClassName("step");
+const topicList = document.getElementsByClassName("topic");
+
 showTab(currentTab)
+
+for (let topic of topicList) {
+    topic.addEventListener("click", event => {
+        if (topic.className.includes(" selected")) {
+            topic.className = topic.className.replace(" selected", "");
+        } else {
+            topic.className += " selected";
+        }
+    });
+}
 
 function showTab(tabNum) {
     // Display the current tab
@@ -26,9 +38,17 @@ function updateStep(tabNum) {
 
     // Add "active" class to current step
     stepList[tabNum].className += " active";
+
+    // Update step text
+    document.getElementById("step-count").textContent = "Step " + (currentTab + 1) + " of 3";
 }
 
 function nextTab() {
+    // Exit the function if any field in the current tab is invalid
+    if (!validateForm()) {
+        return false;
+    }
+
     // Mark previous tab as finished and hide it
     stepList[currentTab].className += " finish";
     tabList[currentTab].style.display = "none";
@@ -44,4 +64,19 @@ function nextTab() {
 
     // Display new tab
     showTab(currentTab);
+}
+
+function validateForm() {
+    let valid = true;
+    const inputList = tabList[currentTab].getElementsByTagName("input");
+
+    if (currentTab === 0) {
+        let re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        valid = inputList[0].value !== "" && re.test(inputList[1].value);
+    }
+    if (currentTab === 1) {
+
+    }
+
+    return valid;
 }
